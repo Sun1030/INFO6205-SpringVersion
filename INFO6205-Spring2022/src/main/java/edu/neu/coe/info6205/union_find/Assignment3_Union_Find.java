27827bb56c349f65400e13296f8257f87a4bf0b3
DamienSun <sun.jiach@northeastern.edu>
2022-03-02 21:07:30 -0500

    Update for Assignment3

--- INFO6205-Spring2022/src/main/java/edu/neu/coe/info6205/union_find/Assignment3_Union_Find.java
+++ INFO6205-Spring2022/src/main/java/edu/neu/coe/info6205/union_find/Assignment3_Union_Find.java
@@ -2,12 +2,24 @@ package edu.neu.coe.info6205.union_find;
 
 public class Assignment3_Union_Find {
 
-    //generate a random number between 0 to i
+    /**
+     * Generate a random number between 0 to i-1
+     *
+     * @param i the max number is i-1
+     * @return a random int between 0 to i-1
+     */
     public static int generateRandomIndex1(int i){
         return (int)(Math.random() * i);
     }
 
-
+    /**
+     * Generates random pairs of integers between 0 and n-1,
+     * calling connected() to determine if they are connected and union() if not.
+     * Loop until all sites are connected then print the number of connections generated.
+     *
+     * @param n the number of "sites"
+     * @return the number of connections generated
+     */
     public static int count(int n){
         int countNum = 0;
         UF_HWQUPC test = new UF_HWQUPC(n);
@@ -22,7 +34,13 @@ public class Assignment3_Union_Find {
         return countNum;
     }
 
-    //Call count() i times when the number if sites is n.
+    /**
+     * Call count() i times when the number of sites is n.
+     *
+     * @param n the number of "sites"
+     * @param i the number of running time
+     * @return the average number of connections generated if running i times
+     */
     public static double doMultiCount(int n, int i){
         double mulCount = 0;
         for(int j = 0; j < i; j++){
